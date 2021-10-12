aadf588b5a5ae868f701d7ea53f006cd67b45a9e
alenka1val <a3valova@gmail.com>
2021-10-12 16:40:20 +0200

    home screen template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,61 @@
 
 @section('content')
 <div class="container">
-    <h1>FitaPixel</h1>
-    <p>It is not a square, it is a sample...</p>
+    <div id="home">
+        <div></div>
+        <div id="left">
+            <h2>Súťaž trvá: </br> od 30.3.2021 </br>do 12.10.2021</h2>
+        </div>
+        <button id="main">
+            Podmienky súťaže
+        </button>
+        <div id="rigth">
+            <h2>It is not a square it is sample</h2>
+        </div>
+        <div></div>
+    </div>
+    <div id="themes" class="themes">
+        <div id="littleThings" class="themeCard">
+            <div class="content">
+                <div>
+                    <h4>Pôvab maličkosti</h4>
+                    <p>
+                        Krása architektonického detailu, čaro živého mikrosveta, pohladenie jemným úsmevom alebo drobným gestom. Živé tvory takisto ako veci môžu mať svoj pôvab. Informačné technológie majú technologickú základňu v mikroelektronike, zdrobneniny sú súčasťou nášho štúdia alebo práce. Vieme ich odhaliť? Odhalíme ich skrytú krásu?
+                    </p>  
+                </div>
+            </div>
+        </div>
+        <div id="nature" class="themeCard">
+            <div class="content">
+                <div>
+                    <h4>Farebná príroda</h4>
+                    <p>
+                        Pri každom pohľade na nejaký kus prírody vidíme nejaké farby. Príroda často dokáže vykúzliť čarokrásne farby alebo ich kombinácie. Obloha, lúka, les, more majú sto rôznych farieb a ich odtieňov. V rôznom ročnom období, v rôznom čase dňa. Pri hre farieb je veľkým kúzelníkom slnko. Jeho lúče súčasne pozlátia kôru stromu a postriebria vlnky potoka. Vytvárajú neopakovateľné a predsa sa opakujúce divadlá farieb sprevádzajúce východ a západ slnka. Dokážeme túto atmosféru zachytiť fotografiou?
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div id="movement" class="themeCard">
+            <div class="content">
+                <div>
+                    <h4>Krása pohybu, čaro okamihu</h4>
+                    <p>  
+                        Pohyb je prejavom života. Tanec, let, skok: mladí, starí, ľudia, živé tvory. Pohyb je aj let oblakov, tečenie potoka či ľadovca. Pokúsme sa vystihnúť krásu pohybu v tom najkrajšom okamihu. Okamih má svoj čaro, aj keď sa práve nič nehýbe. Okamih napätia, dramatickej situácie alebo pohody, pokoja. 
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div id="place" class="themeCard">
+            <div class="content">
+                <div>
+                    <h4>M(i)esto, kde práve som</h4>
+                    <p>
+                        Každé miesto je potenciálnym zdrojom zaujímavých pohľadov. Skúsme odhaliť skryté krásy miest, ktoré bežne navštevujeme alebo ktoré sa nám podarí navštíviť možno raz za život práve teraz. Študujeme v meste, ktoré je síce najmenšou, ale predsa len metropolou Európskej únie. Študujeme na fakulte, ktorá je v doline na kopci. Takže kde: v doline alebo na kopci? Možno už onedlho v novej budove, ktorá je tiež v doline na kopci. Zachyťme, ako vyzerajú miesta, kde prežívame tieto krásne roky svojho života. Zachyťme atmosféru mesta, ktoré práve navštívime, kdekoľvek na zemeguli.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+    <link href="{{ asset('css/stylesheet.css') }}" rel="stylesheet">
 </head>
 <body>
 <header class="app-header">
