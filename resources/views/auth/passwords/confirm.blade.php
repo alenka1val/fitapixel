63a6e7d60a783be7b165db3c0b657cbe7f2faad0
alenka1val <a3valova@gmail.com>
2022-03-13 10:54:44 +0100

    adjust auth blades

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -4,14 +4,14 @@
 <div class="authContainer">
     <div class="authCard">
         <div class="card-body">
-        <h3>{{ __('Confirm Password') }}</h3>
+        <h2>{{ __('Confirm Password') }}</h2>
             {{ __('Please confirm your password before continuing.') }}
             <form method="POST" action="{{ route('password.confirm') }}">
                 @csrf
                 <div class="form-group">
-                    <label for="password">{{ __('Password') }}</label>
+                    <!-- <label for="password">{{ __('Password') }}</label> -->
                     <div>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror input" name="password" placeholder="{{ __('Password') }}" required autocomplete="current-password">
                         @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -24,13 +24,17 @@
                         <button type="submit" class="btn btn-primary authButton">
                             {{ __('Confirm Password') }}
                         </button>
-
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
                     </div>
+                </div>
+                <div class="form-group">
+                    @if (Route::has('password.request'))
+                        <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a> -->
+                        <button class="btn btn-secondary authButton" onClick="window.location='{{ route("password.request") }}'">
+                            {{ __('Forgot Your Password?') }}
+                        </button>
+                    @endif
                 </div>
             </form>
         </div>
