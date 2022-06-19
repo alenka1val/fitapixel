aafb84dd4596d753152d7eda7ca2ee43bbb10b8b
alenka1val <a3valova@gmail.com>
2022-06-19 13:27:01 +0200

    select box for year

--- resources/views/info/themes.blade.php
+++ resources/views/info/themes.blade.php
@@ -8,8 +8,45 @@
         </div> -->
         <div class="pad-right-5p">
             <div>
-                <h2>Témy súťaže</h2>
+                <h2>Galéria</h2>
                 <div class="underline"></div>
+            </div>
+        </div>
+        <div class="filterContainer">
+            <div class="filterCard">
+                <div class="card-body">
+                    <form method="GET">
+                        <div class="form-group">
+                            <!-- <div>
+                                <select class="customSelect" id="cars">
+                                    <option value="volvo">Volvo</option>
+                                    <option value="saab">Saab</option>
+                                    <option value="opel">Opel</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                            </div> -->
+
+                            <div class="dropdown">
+                                <div class="select">
+                                    <span>Vyberte rok</span>
+                                    <i class="fa fa-chevron-left"></i>
+                                </div>
+                                <input type="hidden" name="gender">
+                                <ul class="dropdown-menu">
+                                    <li id="2021">2021</li>
+                                    <li id="2022">2022</li>
+                                </ul>
+                            </div>
+                            <span class="msg"></span>
+                            <input id="year" type="text" name="year" class="hidden">
+                        </div>
+                        <!-- <div clsass="form-group">
+                            <button class="btn btn-secondary authButton" onClick="window.location='{{ route("register") }}'">
+                                Registrácia
+                            </button>
+                        </div> -->
+                    </form>
+                </div>
             </div>
         </div>
         <div class="themes">
@@ -17,7 +54,7 @@
                 <div class="theme-card">
                     <div class="theme-img">
                         <div class="theme-info">
-                            <h3>Téma 1</h3>
+                            <h3>Súťaž 1</h3>
                             <p>
                                 Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
                             </p>
@@ -29,7 +66,7 @@
             <div class="theme-card">
                 <div class="theme-img">
                     <div class="theme-info">
-                        <h3>Téma 1</h3>
+                        <h3>Súťaž 2</h3>
                         <p>
                             Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
                         </p>
@@ -40,7 +77,7 @@
             <div class="theme-card">
                 <div class="theme-img">
                     <div class="theme-info">
-                        <h3>Téma 1</h3>
+                        <h3>Súťaž 3</h3>
                         <p>
                             Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
                         </p>
@@ -51,7 +88,7 @@
             <div class="theme-card">
                 <div class="theme-img">
                     <div class="theme-info">
-                        <h3>Téma 1</h3>
+                        <h3>Súťaž 4</h3>
                         <p>
                             Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
                         </p>
