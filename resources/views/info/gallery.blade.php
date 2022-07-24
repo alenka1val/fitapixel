707eaa47a76a7d052d61870bf571e288c9d7362c
JakubKucecka <jakubkucecka@gmail.com>
2022-07-24 16:48:57 +0200

    Add places to gallery

--- resources/views/info/gallery.blade.php
+++ resources/views/info/gallery.blade.php
@@ -26,7 +26,8 @@
                                         <span>{{ $selected_year }}</span>
                                         <i class="fa fa-chevron-left"></i>
                                     </div>
-                                    <input id="selected_year" type="text" name="selected_year" class="hidden" value="{{ $selected_year }}">
+                                    <input id="selected_year" type="text" name="selected_year" class="hidden"
+                                           value="{{ $selected_year }}">
                                     <ul id="year-list" class="dropdown-menu">
                                         @foreach(array_keys($events) as $year)
                                             <li id="{{$year}}">{{$year}}</li>
@@ -43,7 +44,8 @@
                                         <span id="selected-item-name">{{ $selected_event['name'] }}</span>
                                         <i class="fa fa-chevron-left"></i>
                                     </div>
-                                    <input id="selected_event" type="text" name="selected_event" class="hidden" value="{{ $selected_event['id'] }}">
+                                    <input id="selected_event" type="text" name="selected_event" class="hidden"
+                                           value="{{ $selected_event['id'] }}">
                                     <ul id="event_list" class="dropdown-menu">
                                         @foreach($events[$selected_year] as $event)
                                             <li id="{{$event['id']}}">{{ $event['name'] }}</li>
@@ -58,52 +60,59 @@
                             </button>
                         </div>
                     </form>
-                    
+
                 </div>
             </div>
         </div>
         @if($finished && count($photos)>=3)
             <div class="darkPanel">
-            <h2>Víťazi</h2>
-            <div class="winners">
- 
-                <img id="photoImg-0" class="galery-image first" onclick="zoomIn('photoImg-0', 0 , '{{ $photos[0]->user_name }}' , '{{ $photos[0]->description }}', '{{ $photos[0]->event_id }}')" src="{{$photos[0]->filename}}" alt="{{$photos[0]->description}}"/>
-                <h3 class="first-winner first">{{$photos[0]->user_name}}</h3>
-                <!-- <h4 class="first-winner first">{{$photos[0]->description}}</h4> -->
+                <h2>Víťazi</h2>
+                <div class="winners">
+                    <img id="photoImg-0" class="galery-image first"
+                         onclick="zoomIn('photoImg-0', 0 , '{{ $photos[0]->user_name }}' , '{{ $photos[0]->description }}', '{{ $photos[0]->event_id }}', '{{ $photos[0]->place }}')"
+                         src="{{$photos[0]->filename}}" alt="{{$photos[0]->description}}"/>
+                    <h3 class="first-winner first">{{$photos[0]->user_name}}</h3>
+                    <h4 class="first-winner first"><i>{{$photos[0]->place}}. miesto</i></h4>
 
-                <img id="photoImg-1" class="galery-image second" onclick="zoomIn('photoImg-1', 1 , '{{ $photos[1]->user_name }}' , '{{ $photos[1]->description }}', '{{ $photos[1]->event_id }}')" src="{{$photos[1]->filename}}" alt="{{$photos[1]->description}}"/>
-                <h3 class="second-winner second">{{$photos[1]->user_name}}</h3>
-                <!-- <h4 class="second-winner second">{{$photos[1]->description}}</h4> -->
+                    <img id="photoImg-1" class="galery-image second"
+                         onclick="zoomIn('photoImg-1', 1 , '{{ $photos[1]->user_name }}' , '{{ $photos[1]->description }}', '{{ $photos[1]->event_id }}', '{{ $photos[1]->place }}')"
+                         src="{{$photos[1]->filename}}" alt="{{$photos[1]->description}}"/>
+                    <h3 class="second-winner second">{{$photos[1]->user_name}}</h3>
+                    <h4 class="second-winner second"><i>{{$photos[1]->place}}. miesto</i></h4>
 
-                <img id="photoImg-2" class="galery-image third" onclick="zoomIn('photoImg-2', 2 , '{{ $photos[2]->user_name }}' , '{{ $photos[2]->description }}', '{{ $photos[2]->event_id }}')" src="{{$photos[2]->filename}}" alt="{{$photos[2]->description}}"/>
-                <h3 class="third-winner third">{{$photos[2]->user_name}}</h3>
-                <!-- <h4 class="third-winner third">{{$photos[2]->description}}</h4> -->
+                    <img id="photoImg-2" class="galery-image third"
+                         onclick="zoomIn('photoImg-2', 2 , '{{ $photos[2]->user_name }}' , '{{ $photos[2]->description }}', '{{ $photos[2]->event_id }}', '{{ $photos[2]->place }}')"
+                         src="{{$photos[2]->filename}}" alt="{{$photos[2]->description}}"/>
+                    <h3 class="third-winner third">{{$photos[2]->user_name}}</h3>
+                    <h4 class="third-winner third"><i>{{$photos[2]->place}}. miesto</i></h4>
+                </div>
             </div>
-        </div>
         @endif
         @if(count($photos)>3)
-        <div>
-            <div class="galery">
-                @for ($i = $finished ? 3 : 0; $i < count($photos); $i++)
-                    <img id="photoImg-{{ $i }}" class="galery-image" onclick="zoomIn('photoImg-{{ $i }}', {{ $i }} , '{{ $photos[$i]->user_name }}' , '{{ $photos[$i]->description }}', '{{ $photos[$i]->event_id }}')" src="{{ $photos[$i]->filename }}" alt="{{ $photos[$i]->filename }}"/>
-                @endfor
+            <div>
+                <div class="galery">
+                    @for ($i = $finished ? 3 : 0; $i < count($photos); $i++)
+                        <img id="photoImg-{{ $i }}" class="galery-image"
+                             onclick="zoomIn('photoImg-{{ $i }}', {{ $i }} , '{{ $photos[$i]->user_name }}' , '{{ $photos[$i]->description }}', '{{ $photos[$i]->event_id }}', '{{ $photos[$i]->place }}')"
+                             src="{{ $photos[$i]->filename }}" alt="{{ $photos[$i]->filename }}"/>
+                    @endfor
+                </div>
             </div>
-        </div>
         @endif
         @if($photos)
-        <div id="myModal" class="modal">
-            <span class="close"><i class="fa fa-times zoomImageI"></i></span>
-            <img class="modal-content" id="img01">
-            <div id="caption">
-                <i class="fa fa-arrow-left zoomImageArrows" onclick="moveLeft({{ $photos }})"></i>
-                <div class="modal-div">
-                    <p id="captionText1"></p>
-                    <p id="captionText2"></p>
-                    <!-- <p id="captionText3"></p> -->
+            <div id="myModal" class="modal">
+                <span class="close"><i class="fa fa-times zoomImageI"></i></span>
+                <img class="modal-content" id="img01">
+                <div id="caption">
+                    <i class="fa fa-arrow-left zoomImageArrows left_arrow_move" onclick="moveLeft({{ $photos }})"></i>
+                    <div class="modal-div">
+                        <p id="captionText1"></p>
+                        <p id="captionText2"></p>
+                        <p id="captionText3"></p>
+                    </div>
+                    <i class="fa fa-arrow-right zoomImageArrows right_arrow_move" onclick="moveRight({{ $photos }})"></i>
                 </div>
-                <i class="fa fa-arrow-right zoomImageArrows" onclick="moveRight({{ $photos }})"></i>
             </div>
-        </div>
         @endif
     </div>
     <script>
